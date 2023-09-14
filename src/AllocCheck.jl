1b3699fdf0395570fec1cec7fbdcf825c333f8cb
Gabriel Baraldi <baraldigabriel@gmail.com>
2023-09-14 12:49:48 -0300

    Implement first logic for checking

--- src/AllocCheck.jl
+++ src/AllocCheck.jl
@@ -1,6 +1,7 @@
 module AllocCheck
 
 using GPUCompiler
+using LLVM
 # Write your package code here.
 
 module Runtime end
@@ -16,7 +17,7 @@ runtime_slug(job::NativeCompilerJob) = "native_$(job.config.target.cpu)-$(hash(j
 uses_julia_runtime(job::NativeCompilerJob) = job.config.target.jlruntime
 
 function create_job(@nospecialize(func), @nospecialize(types); entry_abi=:specfunc)
-    source = methodinstance(typeof(func), Base.to_tuple_type(types))
+    source = methodinstance(F, Base.to_tuple_type(types))
     target = DefaultCompilerTarget()
     config = CompilerConfig(target, NativeParams(); kernel=false, entry_abi, always_inline=false)
     CompilerJob(source, config)
@@ -25,8 +26,22 @@ end
 function check_ir(@nospecialize(func), @nospecialize(types); entry_abi=:specfunc)
     job = create_job(func, types; entry_abi)
     ir = JuliaContext() do ctx
-        GPUCompiler.compile(:llvm,job, validate=false)
+        GPUCompiler.compile(:llvm, job, validate=false)
         # Implement checks ;)
+    end
+    mod = ir[1]
+    for f in functions(mod)
+        for block in blocks(f)
+            for inst in instructions(block)
+                if isa(inst, LLVM.CallInst)
+                    decl = called_operand(inst)
+                    if name(decl) == "ijl_gc_pool_alloc"
+                        println("Found Allocation EXTERMINATE!")
+                        println(inst)
+                    end
+                end
+            end
+        end
     end
     ir
 end
